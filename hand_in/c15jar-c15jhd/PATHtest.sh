8e677ca9b0405426c4ebda33d355671b8c8492c1
Jacob Ardnor <c15jar@cs.umu.se>
2019-12-17 17:15:22 +0100

    add custom multi-armed bandit implementation

---
+++ hand_in/c15jar-c15jhd/PATHtest.sh
@@ -0,0 +1,2 @@
+export PATH="$HOME/.local/bin:$PATH"
+pyarmor obfuscate --platform windows.x86_64 --platform linux.x86_64 --platform darwin.x86_64 bandit.py
---
+++ hand_in/c15jar-c15jhd/test/test_runner_notobfuscated.py
@@ -0,0 +1,34 @@
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+sys.path.insert(0, os.path.dirname(parent_dir))
+
+import bandit
+
+def simulate():
+    results = []
+    sim = bandit.simulator.Simulator()
+    
+    for _ in range(0, 20):
+        bandit.ref_bandit.sums = [0] * len(bandit.arms)
+        sim.randomize()
+        bandit_reward = sim.simulate(bandit.bandit)
+        print("Bandit Reward: " + str(bandit_reward))
+        ref_bandit_reward = sim.simulate(bandit.ref_bandit)
+        print("REF Bandit Reward: " + str(ref_bandit_reward))
+        ref_plus_bonus = ref_bandit_reward * 1.20
+        result = 0
+        if (bandit_reward > ref_plus_bonus):
+            result = 1
+        results.append(result)
+    return results
+
+def test_performance():
+    s = sum(simulate())
+    print(s)
+    assert s > 17
+    
+
+
+test_performance()
