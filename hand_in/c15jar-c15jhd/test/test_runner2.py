5a4f59a83d142ca7faf1290de75acc48e35cafe7
Jacob Ardnor <c15jar@cs.umu.se>
2019-12-18 11:52:25 +0100

    new try

--- hand_in/c15jar-c15jhd/test/test_runner2.py
+++ hand_in/c15jar-c15jhd/test/test_runner2.py
@@ -8,9 +8,15 @@ import bandit
 
 def simulate():
     results = []
+    sim = bandit.simulator.Simulator()
+    
     for _ in range(0, 20):
-        bandit_reward = bandit.simulator.simulate(bandit.bandit)
-        ref_bandit_reward = bandit.simulator.simulate(bandit.ref_bandit)
+        bandit.ref_bandit.sums = [0] * len(bandit.arms)
+        sim.randomize()
+        bandit_reward = sim.simulate(bandit.bandit)
+        print("Bandit Reward: " + str(bandit_reward))
+        ref_bandit_reward = sim.simulate(bandit.ref_bandit)
+        print("REF Bandit Reward: " + str(ref_bandit_reward))
         ref_plus_bonus = ref_bandit_reward * 1.05
         result = 0
         if (bandit_reward > ref_plus_bonus):
@@ -19,5 +25,6 @@ def simulate():
     return results
 
 def test_performance():
-    assert True
-    # assert sum(simulate()) > 17
+    s = sum(simulate())
+    print(s)
+    assert s > 17
